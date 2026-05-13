ae9a7284214b9538f29e674c48bf15789fdf3c38
yashm <yashm@users.noreply.github.com>
2026-05-13 10:18:44 +0530

    Fix: Forced IPv4 to resolve network unreachable errors

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,12 @@
 import os
 import sys
+import socket
+
+# Force IPv4 to resolve "Network is unreachable" errors in IPv6-hostile environments
+old_getaddrinfo = socket.getaddrinfo
+def force_ipv4(host, port, family=0, type=0, proto=0, flags=0):
+    return old_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
+socket.getaddrinfo = force_ipv4
 
 # Force the current directory into the python path for Docker/HuggingFace
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
